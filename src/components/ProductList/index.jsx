f4251a49f6692050a52f5fe81e88ea8dcdd4ca6a
bervalim <bernardogvalim@gmail.com>
2023-08-03 12:54:21 -0300

    kenzieBurguer

--- src/components/ProductList/index.jsx
+++ src/components/ProductList/index.jsx
@@ -11,24 +11,21 @@ export const ProductList = ({
     <>
       {searchProducts ? (
         <>
-          <h3>Resultados de busca para:{searchProducts}</h3>
+          <h3>Resultados de busca para: {searchProducts}</h3>
           <p>{productsListExibition.length} produto(s) foram encontrados</p>
           <button onClick={clearFilter}>Limpar Filtro</button>
         </>
       ) : null}
-      {productsListExibition.length > 0 ? (
-        <ul>
-          {productsListExibition.map((product) => (
-            <ProductCard
-              key={product.id}
-              product={product}
-              addProductToCart={addProductToCart}
-            />
-          ))}
-        </ul>
-      ) : (
-        <h2>Nenhum produto encontrado</h2>
-      )}
+
+      <ul>
+        {productsListExibition.map((product) => (
+          <ProductCard
+            key={product.id}
+            product={product}
+            addProductToCart={addProductToCart}
+          />
+        ))}
+      </ul>
     </>
   );
 };
